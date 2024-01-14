160377aebdbaf8839c610c5eed4f900be9895aee
benj631 <benjacobs@byui.edu>
2024-01-13 19:58:47 -0700

    added Prep1 and Prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is the grade in a percent?");
+        string gradeString = Console.ReadLine();
+        int grade = int.Parse(gradeString);
+        string gradeLetter = null;
+
+        if (grade >= 90){
+            gradeLetter = "A";
+        } else if (grade >= 80) {
+            gradeLetter = "B";
+        } else if (grade >= 70) {
+            gradeLetter = "C";
+        } else if (grade >= 60) {
+            gradeLetter = "D";
+        } else if (grade < 60) {
+            gradeLetter = "F";
+        }
+        
+        Console.WriteLine($"Your grade letter is {gradeLetter}");
+
     }
 }

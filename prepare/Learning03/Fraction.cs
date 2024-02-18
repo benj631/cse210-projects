800c627e64a177457a8a226665cabd6e93aacc53
benj631 <benjacobs@byui.edu>
2024-02-17 17:03:13 -0700

    Learning 03 Completed

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,41 @@
+using System;
+
+class Fraction {
+    private int numerator;
+    private int denominator;
+
+    public Fraction(int top = 0, int bot = 1) {
+        this.numerator = top;
+        if (bot == 0) {
+            throw new ArgumentException("Denominator cannot be zero.");
+        }
+        this.denominator = bot;
+    }
+
+    public void SetTop(int value) {
+        this.numerator = value;
+    }
+
+    public void SetBottom(int value) {
+        if (value == 0) {
+            throw new ArgumentException("Denominator cannot be zero.");
+        }
+        this.denominator = value;
+    }
+
+    public int GetTop() {
+        return this.numerator;
+    }
+
+    public int GetBottom() {
+        return this.denominator;
+    }
+
+    public string GetFractionString() {
+        return $"{this.numerator}/{this.denominator}";
+    }
+
+    public float GetDecimalValue() {
+        return (float)this.numerator / this.denominator;
+    }
+}
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+    Fraction myFract = new Fraction();
+    Fraction myFract2 = new Fraction(5);
+    Fraction myFract3 = new Fraction(5,2);
+
+    Console.WriteLine($"myFract: {myFract.GetFractionString()}, myFract2: {myFract2.GetFractionString()}, myFract3: {myFract3.GetFractionString()} ");
+    Console.WriteLine($"myFract: {myFract.GetDecimalValue()}, myFract2: {myFract2.GetDecimalValue()}, myFract3: {myFract3.GetDecimalValue()} ");
+
     }
 }
